60ee46c5da7cf07f9aaeb3eca317c04520186a18
alperenonarir <alperenonarir@gmail.com>
2023-01-06 15:23:37 +0300

    psvm eklendi

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
 // Visual Studio Code Java Extension Test
 import java.util.Scanner; // Scanner remote üzerinden dahil edildi
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        
+    }
+
+   
+}
